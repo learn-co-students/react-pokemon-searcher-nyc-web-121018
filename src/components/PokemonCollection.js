835860aa8d8b18df5eff2cbc52b2dbea6db0b3c5
Nison Malayev <nmalayev@gmail.com>
2019-02-26 17:29:21 -0500

    Done.

--- src/components/PokemonCollection.js
+++ src/components/PokemonCollection.js
@@ -1,15 +1,44 @@
-import React from 'react'
-import PokemonCard from './PokemonCard'
-import { Card } from 'semantic-ui-react'
+import React from 'react';
+import PokemonCard from './PokemonCard';
+import { Card } from 'semantic-ui-react';
 
 class PokemonCollection extends React.Component {
+  renderAllPokemon = () => {
+    let render = this.props.pokemon.map(p => {
+      return (
+        <PokemonCard
+          key={p.id}
+          name={p.name}
+          sprites={p.sprites}
+          hp={p.stats.find(stat => stat.name === 'hp').value}
+        />
+      );
+    });
+
+    if (this.props.search !== '') {
+      return this.props.pokemon
+        .filter(p =>
+          p.name.toLowerCase().includes(this.props.search.toLowerCase())
+        )
+        .map(p => {
+          return (
+            <PokemonCard
+              key={p.id}
+              name={p.name}
+              sprites={p.sprites}
+              hp={p.stats.find(stat => stat.name === 'hp').value}
+            />
+          );
+        });
+    } else {
+      return render;
+    }
+  };
+
+  renderSearchedPokemon = () => {};
   render() {
-    return (
-      <Card.Group itemsPerRow={6}>
-        <h1>Hello From Pokemon Collection</h1>
-      </Card.Group>
-    )
+    return <Card.Group itemsPerRow={6}>{this.renderAllPokemon()}</Card.Group>;
   }
 }
 
-export default PokemonCollection
+export default PokemonCollection;
