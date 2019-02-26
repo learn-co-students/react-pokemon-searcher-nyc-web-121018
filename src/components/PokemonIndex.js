ceb3c586e5dbec030758c90383a626c67f358df6
tfuquirk <timothyfquirk@gmail.com>
2019-02-25 19:51:40 -0500

    Done.

--- src/components/PokemonIndex.js
+++ src/components/PokemonIndex.js
@@ -1,23 +1,61 @@
-import React from 'react'
-import PokemonCollection from './PokemonCollection'
-import PokemonForm from './PokemonForm'
-import { Search } from 'semantic-ui-react'
-import _ from 'lodash'
+import React from "react";
+import PokemonCollection from "./PokemonCollection";
+import PokemonForm from "./PokemonForm";
+import { Search } from "semantic-ui-react";
+import _ from "lodash";
 
 class PokemonPage extends React.Component {
+  state = {
+    pokemon: [],
+    search: ""
+  };
+
+  componentDidMount() {
+    fetch("http://localhost:3000/pokemon")
+      .then(resp => resp.json())
+      .then(pokedata => {
+        this.setState({ pokemon: pokedata });
+      });
+  }
+
+  filterPokemon = event => {
+    if (this.state.search === "") {
+      return this.state.pokemon;
+    } else {
+      return this.state.pokemon.filter(pokemon => {
+        return pokemon.name.includes(this.state.search.toLowerCase());
+      });
+    }
+  };
+
+  handleSearchChange = (e, { value }) => {
+    this.setState({
+      search: value
+    });
+  };
+
+  createPokemon = pokemon => {
+    this.setState({ pokemon: [...this.state.pokemon, pokemon] });
+  };
+
   render() {
+    console.log(this.state.pokemon);
     return (
       <div>
         <h1>Pokemon Searcher</h1>
         <br />
-        <Search onSearchChange={_.debounce(() => console.log('🤔'), 500)} showNoResults={false} />
+        <h3>Search by name:</h3>
+        <Search
+          onSearchChange={_.debounce(this.handleSearchChange, 500)}
+          showNoResults={false}
+        />
         <br />
-        <PokemonCollection />
+        <PokemonCollection pokemon={this.filterPokemon()} />
         <br />
-        <PokemonForm />
+        <PokemonForm createPokemon={this.createPokemon} />
       </div>
-    )
+    );
   }
 }
 
-export default PokemonPage
+export default PokemonPage;
